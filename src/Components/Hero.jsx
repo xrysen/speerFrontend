e9df0ffff2a8c7cfefb9fb836d09c0e88948ead5
Sean Oyler <xrysen@gmail.com>
2021-05-09 12:52:12 -0700

    Adds react-bootstrap

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,13 +2,13 @@ import "./Hero.css";
 import { useEffect, useState } from "react";
 
 const Hero = () => {
-  const [currImage, setCurrImage] = useState(0);
+  const [currImage, setCurrImage] = useState(2);
 
   const images = [
     "https://cdn.pixabay.com/photo/2015/05/15/14/50/concert-768722_960_720.jpg",
     "https://cdn.pixabay.com/photo/2016/11/29/06/17/audience-1867754_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2016/11/18/15/44/audience-1835431_960_720.jpg"
-  ]
+    "https://cdn.pixabay.com/photo/2016/11/18/15/44/audience-1835431_960_720.jpg",
+  ];
 
   // useEffect(() => {
   //   setTimeout(() => {
@@ -21,10 +21,19 @@ const Hero = () => {
   // }, [currImage])
 
   return (
-    <div className = "hero-container" style = {{ backgroundImage: `url(${images[currImage]})`}}>
-      {/* <img className = "hero-image" src = "https://cdn.pixabay.com/photo/2015/05/15/14/50/concert-768722_960_720.jpg" alt="Concert Pic" /> */}
+    <div
+      className="hero-container"
+      style={{ backgroundImage: `url(${images[currImage]})` }}
+    >
+      <div className="hero-text">
+        <h1>INTERACTIVE CONCERT EXPERIENCE</h1>
+        <p>
+          Experience your favourite artists like never before and from the
+          comfort of your own home.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Hero;
