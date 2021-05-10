90fd1305d0285c6caf336bd56e7b617e701a07af
Sean Oyler <xrysen@gmail.com>
2021-05-09 18:24:19 -0700

    Sets up yellow section

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import './App.css';
 import Hero from "./Components/Hero";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SectionRed from "./Components/SectionRed";
+import SectionYellow from "./Components/SectionYellow";
 
 function App() {
   return (
     <div className="App">
       <SectionRed />
-      <div>Lorem Impsim</div>
-      <div>Lorem Impsim</div>
-      <div>Lorem Impsim</div>
-      <div>Lorem Impsim</div>
-      <div>Lorem Impsim</div>
+      <SectionYellow />
     </div>
   );
 }
---
+++ src/Components/SectionYellow.jsx
@@ -0,0 +1,7 @@
+import "./SectionYellow.css";
+
+const SectionYellow = () => {
+  return <div className="yellow-container"></div>;
+};
+
+export default SectionYellow;
