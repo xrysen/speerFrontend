49c812c24f1e2e5ac6772fce609f2c49f3aebcd2
Sean Oyler <xrysen@gmail.com>
2021-05-09 22:41:10 -0700

    Fixes positioning of content in getNow component

--- src/Components/GetNow.jsx
+++ src/Components/GetNow.jsx
@@ -5,13 +5,17 @@ import LiquidButton from "./LiquidButton";
 const GetNow = () => {
   return (
     <div className="get-now-container">
-      <NewBurger liColour="yellow-li" />
+      <div className="get-now-nav">
+        <NewBurger liColour="yellow-li" />
+      </div>
       <div className="get-now-content-container">
         <div className="get-now-content">
-          <h1 style = {{ color: "#D34848"}}>GET EXP|CON NOW</h1>
-          <p style = {{ color: "white"}}>Purchase and download the app.</p>
+          <h1 style={{ color: "#D34848" }}>GET EXP|CON NOW</h1>
+          <p style={{ color: "white", fontSize: "30px" }}>
+            Purchase and download the app.
+          </p>
         </div>
-        <LiquidButton label = "TRY IT NOW" />
+        <LiquidButton label="TRY IT NOW" />
       </div>
     </div>
   );
