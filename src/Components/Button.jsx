9d81dfde71f4439cafe4ced266fe82bbc577f39f
Sean Oyler <xrysen@gmail.com>
2021-05-09 17:13:20 -0700

    Adds button, still need to work on effect

---
+++ src/Components/Button.jsx
@@ -0,0 +1,19 @@
+import "./Button.css";
+import { useState } from "react";
+
+const Button = (props) => {
+  const [background, setBackground] = useState(props.background);
+  const [textColour, setTextColour] = useState(props.text);
+  return (
+    <div
+      className="button"
+      style={{ background: background, color: textColour }}
+      onMouseEnter={() => { setBackground("linear-gradient(to right, #667eea, #764ba2, #6B8DD6, #8E37D7)"); setTextColour("white");}}
+      onMouseLeave={() => { setBackground(props.background); setTextColour(props.text)}}
+    >
+      {props.label}
+    </div>
+  );
+};
+
+export default Button;
--- src/Components/SectionRed.jsx
+++ src/Components/SectionRed.jsx
@@ -1,12 +1,16 @@
 import "./SectionRed.css";
 import Burger from "./Burger";
+import Button from "./Button";
 
 const SectionRed = () => {
   return (
-    <div className = "red-container">
+    <div className="red-container">
       <Burger />
+      <div className="top-button">
+        <Button label = "TRY IT NOW" background = "white" text = "#D34848" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default SectionRed;
