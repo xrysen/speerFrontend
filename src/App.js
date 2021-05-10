4081cf43ad571b5d03b4dced1c5308d2e7a38baf
Sean Oyler <xrysen@gmail.com>
2021-05-09 21:05:00 -0700

    Sets up perks sections

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Hero from "./Components/Hero";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SectionRed from "./Components/SectionRed";
 import SectionYellow from "./Components/SectionYellow";
+import Perks from "./Components/Perks";
 
 function App() {
   return (
     <div className="App">
       <SectionRed />
       <SectionYellow />
+      <Perks />
     </div>
   );
 }
---
+++ src/Components/Perks.jsx
@@ -0,0 +1,12 @@
+import "./Perks.css";
+import TopNav from "./TopNav";
+
+const Perks = () => {
+  return (
+    <div className = "black-container">
+      <TopNav liColour = "yellow-li" />
+    </div>
+  )
+}
+
+export default Perks;
