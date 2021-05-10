8b591b72b323318affb0e24d110f7e0dba5addf9
Sean Oyler <xrysen@gmail.com>
2021-05-09 21:53:44 -0700

    Sets up review container

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import SectionRed from "./Components/SectionRed";
 import SectionYellow from "./Components/SectionYellow";
 import Perks from "./Components/Perks";
+import Reviews from "./Components/Reviews";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <SectionRed />
       <SectionYellow />
       <Perks />
+      <Reviews />
     </div>
   );
 }
---
+++ src/Components/Reviews.jsx
@@ -0,0 +1,12 @@
+import "./Reviews.css";
+import TopNav from "./TopNav";
+
+const Reviews = () => {
+  return (
+    <div className = "reviews-container">
+      <TopNav bgColour = "#1FE1E9" liColour = "blue-li" />
+    </div>
+  )
+}
+
+export default Reviews;
