9fb700d78bbfdde4d6c39d15972f0cc54ffc914e
Sean Oyler <xrysen@gmail.com>
2021-05-09 18:36:30 -0700

    May have to revert...

--- src/Components/SectionYellow.jsx
+++ src/Components/SectionYellow.jsx
@@ -1,7 +1,10 @@
 import "./SectionYellow.css";
+import Burger2 from "./Burger2";
 
 const SectionYellow = () => {
-  return <div className="yellow-container"></div>;
+  return <div className="yellow-container">
+    <Burger2 />
+  </div>;
 };
 
 export default SectionYellow;
