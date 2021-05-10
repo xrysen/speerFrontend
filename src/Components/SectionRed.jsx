aae8277e4045cbd377668570c46a42b2ad61a643
Sean Oyler <xrysen@gmail.com>
2021-05-09 17:25:31 -0700

    Adds info and speakers

--- src/Components/SectionRed.jsx
+++ src/Components/SectionRed.jsx
@@ -1,4 +1,5 @@
 import "./SectionRed.css";
+import speakers from "../assets/speakers.png";
 import Burger from "./Burger";
 import Button from "./Button";
 
@@ -8,6 +9,16 @@ const SectionRed = () => {
       <Burger />
       <div className="top-button">
         <Button label = "TRY IT NOW" background = "white" text = "#D34848" />
+      </div>
+      <div className = "content-container">
+        <div className = "info">
+          <h1>SUPERIOR SOUND</h1>
+          <p>Experience live versions of your favourite songs.</p>
+          <Button label = "SEE DEMO" background = "white" text = "#D34848" />
+        </div>
+        <div className = "speakers">
+          <img src={speakers} alt="speakers" />
+        </div>
       </div>
     </div>
   );
