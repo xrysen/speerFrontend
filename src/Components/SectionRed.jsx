1516d2040cb63369c26891562ab5d62dfec68bca
Sean Oyler <xrysen@gmail.com>
2021-05-09 17:54:55 -0700

    Adds ability to play and pause music

--- src/Components/SectionRed.jsx
+++ src/Components/SectionRed.jsx
@@ -1,24 +1,40 @@
+import { useState, useRef } from "react";
 import "./SectionRed.css";
 import speakers from "../assets/speakers.png";
 import Burger from "./Burger";
 import Button from "./Button";
+import music from "../assets/music/island-music.mp3";
 
 const SectionRed = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const ref = useRef();
+
+  const playSong = () => {
+    ref.current.play();
+    setIsPlaying(true);
+  }
+
+  const pauseSong = () => {
+    ref.current.pause();
+    setIsPlaying(false);
+  }
+
   return (
     <div className="red-container">
       <Burger />
       <div className="top-button">
-        <Button label = "TRY IT NOW" background = "white" text = "#D34848" />
+        <Button label="TRY IT NOW" background="white" text="#D34848" />
       </div>
-      <div className = "content-container">
-        <div className = "info">
+      <div className="content-container">
+        <div className="info">
           <h1>SUPERIOR SOUND</h1>
           <p>Experience live versions of your favourite songs.</p>
-          <Button label = "SEE DEMO" background = "white" text = "#D34848" />
+          <Button label="SEE DEMO" background="white" text="#D34848" />
         </div>
-        <div className = "speakers">
+        <div className="speakers">
           <img src={speakers} alt="speakers" />
-          <div className = "music-start">
+          <div className="music-start" onClick = {isPlaying ? pauseSong : playSong}> 
+           <audio ref = {ref} src = {music} />
             CLICK
           </div>
         </div>
